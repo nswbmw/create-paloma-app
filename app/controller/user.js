8444c013bd122cf9451299d6f98e2737c57e0780
nswbmw <gxqzk@126.com>
2018-12-27 15:31:41 +0800

    fix: use tracer2

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,5 +1,7 @@
-app.controller('User.getUser', async (ctx, next, User) => {
+app.controller('User.getUser', async (ctx, next, logger, User) => {
   const uid = ctx.params.uid
   const user = await User.getUserByUid(uid)
+
+  logger.info(`User.getUser -> uid: %s, user: %j`, uid, user)
   ctx.body = user
 })
--- app/lib/logger.js
+++ app/lib/logger.js
@@ -1,4 +1,7 @@
+const path = require('path')
 const config = require('config-lite')(__dirname)
-const tracer = require('tracer')
+const tracer = require('tracer2')
 
-module.exports = tracer.colorConsole(config.logger)
+module.exports = tracer.colorConsole(Object.assign({
+  rootDir: path.join(__dirname, '../..')
+}, config.logger))
